d1f1278aa599cdc0346c63659fa4e5d812c27789
Princess Sampson <ps@kingonomics.com>
2016-08-09 17:22:08 -0400

    Initial commit

--- OlympicsRPG/main.c
+++ OlympicsRPG/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int scoreTiers[] = {33, 66, 99};
+
+int eventScore() {
+    
+    return scoreTiers[arc4random_uniform(2)];
+    
+}
+
+double calculateRawCompositeScore(int scores[], int scoreSize) {
+    
+    double rtn = 0;
+    
+    for (int i = 0; i<scoreSize; i++) {
+        rtn += scores[i];
+    }
+    
+    return rtn / 6;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
     return 0;
+    
 }
