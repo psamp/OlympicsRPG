5d0a4347f4e57a2808658a79930c73ddc0c9f456
Princess Sampson <ps@kingonomics.com>
2016-08-09 16:50:53 -0400

    Initial Commit

---
+++ OlympicsRPG/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OlympicsRPG
+//
+//  Created by Princess Sampson on 8/9/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
