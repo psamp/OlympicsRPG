24235f12b294044df04db6e6ba276a0e56e78232
Princess Sampson <ps@kingonomics.com>
2016-08-09 23:42:04 -0400

    Allow the first two cases to exist

--- OlympicsRPG/main.c
+++ OlympicsRPG/main.c
@@ -8,30 +8,64 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int scoreTiers[] = {33, 66, 99};
+int sports = 6;
 
+void newGame();
 int generateRandomEventScore();
 int calcAbsoluteDifference(int, int);
-double calculateAverageScore(int[], int);
+double calculateAverageScore(int);
 int calculateWeightedAverageScore(double);
 int indexOfSmallestArrayValue(int[], int arrSize);
 
 int main(int argc, const char * argv[]) {
     
-    int rawScores[6];
+    newGame();
+}
+
+void newGame() {
     
-    for (int i = 0; i <= 6; i++) {
-        rawScores[i] = generateRandomEventScore();
+    int score = 0;
+    int finalWeighedScore = 0;
+    
+    bool running = true;
+    int option = -1;
+    
+    while (running) {
+        printf("\nSwim (1\nGymnastics (bar) (2\nArchery (3)\nSkating( 4\nTrack (5\nFencing (6\n");
+        scanf("%d", &option);
+        
+        switch (option) {
+            case 1: {
+                
+                printf("You pull your goggles down over your eyes and try to tune out the roaring crowd noise, the clicks of the phototgraphers's camera. _This is it_, you think. The horn blares out, and you're in the water, kicking faster htan you ever have.\n");
+                
+                score += generateRandomEventScore();
+                
+                printf("YOUR SCORE IS: %d\n", score);
+                
+                break;
+            }
+            case 2: {
+                
+                printf("You rub chalk on your hands until your palms are coated in it, clap them together to shake off the excess. It's time. You run, launch yourself off the ground. Heart beating and adrenaline racing, you know the photographers have frozen in time the almost invisble cloud of chalkdust that puffs up when your hands hit the bar. \n");
+                
+                score += generateRandomEventScore();
+                
+                printf("YOUR SCORE IS: %d\n", score);
+                
+                break;
+            }
+            default:
+                break;
+        }
+        
+        running = false;
+        
     }
-    
-    double rawCompScore = calculateAverageScore(rawScores, 6);
-    
-    printf("raw score: %f\n", rawCompScore);
-    
-    printf("calculate weighted comp score: %d\n", calculateWeightedAverageScore(rawCompScore));
-    
-    return 0;
+
 }
 
 int generateRandomEventScore() {
@@ -55,28 +89,22 @@ int indexOfSmallestArrayValue(int arr[], int arrSize){
     return index;
 }
 
-double calculateAverageScore(int scores[], int scoresSize) {
+double calculateAverageScore(int score) {
     
-    double rtn = 0;
-    
-    for (int i = 0; i<scoresSize; i++) {
-        rtn += scores[i];
-    }
-    
-    return rtn / 6;
+    return score / sports;
 }
 
 
-int calculateWeightedAverageScore(double rawCompositeScore) {
+int calculateWeightedAverageScore(double rawAverageScore) {
     
     int differencesBetweenAverageScoreAndScoreTiers[3];
     
     for (int i = 0; i < 3; i++) {
-        differencesBetweenAverageScoreAndScoreTiers[i] = calcAbsoluteDifference(rawCompositeScore,scoreTiers[i]);
+        differencesBetweenAverageScoreAndScoreTiers[i] = calcAbsoluteDifference(rawAverageScore,scoreTiers[i]);
     }
     
-    int weightedCompositeScore = scoreTiers[indexOfSmallestArrayValue(differencesBetweenAverageScoreAndScoreTiers, 3)];
+    int weightedAverageScore = scoreTiers[indexOfSmallestArrayValue(differencesBetweenAverageScoreAndScoreTiers, 3)];
     
     
-    return weightedCompositeScore;
+    return weightedAverageScore;
 }
